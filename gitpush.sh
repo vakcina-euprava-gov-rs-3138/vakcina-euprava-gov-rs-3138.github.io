2522930da9a8a61081eafc6eb070f88f99b353ba
united <chaoabordo@gmail.com>
2022-01-16 17:51:55 +0100

    Test

--- gitpush.sh
+++ gitpush.sh
@@ -2,8 +2,8 @@
 github_user=$1
 github_url="https://github.com/"$github_user"/"$github_user".github.io/"
 #github_url="https://github.com/vakcina-euprava-gov-rs-3136/vakcina-euprava-gov-rs-3136.github.io"
-git remote add origin $github_url
 git init
+git remote add origin $github_url
 git add -A
 git branch -M main
 git commit -m 'Test'
